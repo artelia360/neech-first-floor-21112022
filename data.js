68e514795eca71a06c36a5cf9f0642b6b75038fa
STEVENS Connie <connie.stevens@arteliagroup.com>
2022-11-25 12:00:41 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,791 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-corridor-1",
+      "name": "Corridor 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.12976143569174425,
+        "pitch": -0.005959843112378849,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18699261813877577,
+          "pitch": 0.09454954746909117,
+          "rotation": 0,
+          "target": "3-corridor-2"
+        },
+        {
+          "yaw": 2.4777739962880023,
+          "pitch": 0.11984492095571753,
+          "rotation": 0,
+          "target": "1-room-1"
+        },
+        {
+          "yaw": -1.7692856830030657,
+          "pitch": 0.2565743778082332,
+          "rotation": 0,
+          "target": "2-room-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-room-1",
+      "name": "Room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.0596622692055568,
+        "pitch": 0.12584438435212242,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.686149681999262,
+          "pitch": 0.14459778360494724,
+          "rotation": 0,
+          "target": "0-corridor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-room-2",
+      "name": "Room 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.183255292555625,
+        "pitch": 0.007830648204166124,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.211878636150707,
+          "pitch": 0.10124832699383468,
+          "rotation": 0,
+          "target": "0-corridor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-corridor-2",
+      "name": "Corridor 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.391646399228442,
+        "pitch": 0.01494580743440821,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5339852182271763,
+          "pitch": 0.11140481197340613,
+          "rotation": 0,
+          "target": "5-corridor-3"
+        },
+        {
+          "yaw": 1.5624538109062502,
+          "pitch": 0.12286648000335632,
+          "rotation": 0,
+          "target": "0-corridor-1"
+        },
+        {
+          "yaw": 2.499250032604273,
+          "pitch": 0.1195268428219336,
+          "rotation": 0,
+          "target": "4-room-3"
+        },
+        {
+          "yaw": -1.018403055072433,
+          "pitch": 0.141518168356356,
+          "rotation": 0,
+          "target": "14-corridor-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-room-3",
+      "name": "Room 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 1.477014390152374,
+        "pitch": 0.002430510107931383,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.111633802748626,
+          "pitch": 0.13183605086798522,
+          "rotation": 0,
+          "target": "3-corridor-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-corridor-3",
+      "name": "Corridor 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.1333034368001051,
+        "pitch": 0.036635506190791034,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2161779870748752,
+          "pitch": 0.09273793943646425,
+          "rotation": 0,
+          "target": "10-corridor-4"
+        },
+        {
+          "yaw": 1.9271999365216086,
+          "pitch": 0.10811814885721205,
+          "rotation": 0,
+          "target": "3-corridor-2"
+        },
+        {
+          "yaw": 2.4262799826328516,
+          "pitch": 0.1610671061492539,
+          "rotation": 0,
+          "target": "6-room-4"
+        },
+        {
+          "yaw": -1.6552687674873017,
+          "pitch": 0.1327396701087995,
+          "rotation": 0,
+          "target": "8-room-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-room-4",
+      "name": "Room 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.2065537002917353,
+        "pitch": -0.04180158037589443,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9505462427905673,
+          "pitch": 0.17699152646077287,
+          "rotation": 0,
+          "target": "5-corridor-3"
+        },
+        {
+          "yaw": -1.6442283475663828,
+          "pitch": 0.24522818494185472,
+          "rotation": 0,
+          "target": "7-room-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-room-5",
+      "name": "Room 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.4981545028415013,
+        "pitch": 0.01854704837872667,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3078334956345854,
+          "pitch": 0.1446343466714506,
+          "rotation": 0,
+          "target": "6-room-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-room-6",
+      "name": "Room 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.6323516985441735,
+        "pitch": 0.18599408089672664,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7701346369151683,
+          "pitch": 0.1913170545152738,
+          "rotation": 0,
+          "target": "5-corridor-3"
+        },
+        {
+          "yaw": 0.5708589024275774,
+          "pitch": 0.11401141046692942,
+          "rotation": 0,
+          "target": "9-room-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-room-7",
+      "name": "Room 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.1226582371490395,
+        "pitch": -0.01966907833115883,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1353135948358464,
+          "pitch": 0.18873144189561764,
+          "rotation": 0,
+          "target": "8-room-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-corridor-4",
+      "name": "Corridor 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.854724086474647,
+        "pitch": 0.10970310933261374,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3194752372119707,
+          "pitch": 0.0935312288187653,
+          "rotation": 0,
+          "target": "5-corridor-3"
+        },
+        {
+          "yaw": -0.8871740157112828,
+          "pitch": 0.14351244228183369,
+          "rotation": 0,
+          "target": "12-corridor-5"
+        },
+        {
+          "yaw": 0.695445052074021,
+          "pitch": 0.18503082482562938,
+          "rotation": 0,
+          "target": "11-room-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-room-8",
+      "name": "Room 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.0725124264613655,
+        "pitch": 0.06599532662207963,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8289062507211922,
+          "pitch": 0.07558170186647395,
+          "rotation": 0,
+          "target": "10-corridor-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-corridor-5",
+      "name": "Corridor 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.9189138022745347,
+        "pitch": 0.21001901154220093,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.253396171270264,
+          "pitch": 0.09881484343346969,
+          "rotation": 0,
+          "target": "10-corridor-4"
+        },
+        {
+          "yaw": -2.2458679632318237,
+          "pitch": 0.1338662822920007,
+          "rotation": 0,
+          "target": "13-room-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-room-9",
+      "name": "Room 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.7321439553408595,
+        "pitch": 0.11122485141795835,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6885466984077215,
+          "pitch": 0.10105264560464633,
+          "rotation": 0,
+          "target": "12-corridor-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-corridor-6",
+      "name": "Corridor 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.0793807066155487,
+        "pitch": 0.09907277341728005,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1947031118576703,
+          "pitch": 0.05315326169414192,
+          "rotation": 0,
+          "target": "3-corridor-2"
+        },
+        {
+          "yaw": -1.071538867256919,
+          "pitch": 0.1856587393759579,
+          "rotation": 0,
+          "target": "15-corridor-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-corridor-7",
+      "name": "Corridor 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.802883627653415,
+        "pitch": 0.058494037528852516,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3293825021765198,
+          "pitch": 0.08003169150428491,
+          "rotation": 0,
+          "target": "14-corridor-6"
+        },
+        {
+          "yaw": 0.41550056248376777,
+          "pitch": 0.15070102826853038,
+          "rotation": 0,
+          "target": "16-room-10"
+        },
+        {
+          "yaw": -2.140556157826829,
+          "pitch": 0.13370423962377487,
+          "rotation": 0,
+          "target": "17-room-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-room-10",
+      "name": "Room 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.9447543124925808,
+        "pitch": 0.02979921556189069,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.33954710028552,
+          "pitch": 0.15875820673367436,
+          "rotation": 0,
+          "target": "15-corridor-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-room-11",
+      "name": "Room 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.7538297130590372,
+        "pitch": -0.010019393081808659,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4533442263191807,
+          "pitch": 0.15640152327360823,
+          "rotation": 0,
+          "target": "15-corridor-7"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "NEECH First Floor 21/11/2022",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
